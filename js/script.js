5106b51fce8d4a667303202482d09a1ec41ed4b1
V Vishu <vishu16384@gmail.com>
2025-12-23 08:55:33 +0530

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function showMessage() {
+document.getElementById("explore").addEventListener("click", event => {
     document.getElementById("message").innerText = "Welcome! Event registration will open soon.";
     document.querySelector("body").style.backgroundColor = "#2e88f0ff"
-}
+})
