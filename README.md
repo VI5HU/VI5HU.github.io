a03146f93692de823ad6af965abab6a2dca3bba4
V Vishu <vishu16384@gmail.com>
2025-12-23 08:26:48 +0530

    minor changes in js and readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is the readme for a github pages demonstration
+
+Hosted at [vi5hu.github.io](https://vi5hu.github.io/)
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 function showMessage() {
-    document.getElementById("message").innerText =
-        "Welcome! Event registration will open soon.";
+    document.getElementById("message").innerText = "Welcome! Event registration will open soon.";
+    document.querySelector("body").style.backgroundColor = "#2e88f0ff"
 }
